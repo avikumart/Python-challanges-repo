e9d40edf396f9fc69748eae3dcb515d823846b27
AVIKUMAR TALAVIYA <88608935+avikumart@users.noreply.github.com>
2022-06-17 18:56:41 +0530

    Add files via upload

---
+++ Nested -if-else statements.py
@@ -0,0 +1,14 @@
+a=22
+b=-25
+
+if a>0:
+    if b>0:
+        print("A is positive and B is positive")
+    else:
+        print('A is positive and B is negetive')
+else:
+    if b>0:
+        print("A is negative and B is positive")
+    else:
+        print("A is negative and B is negative")
+        
